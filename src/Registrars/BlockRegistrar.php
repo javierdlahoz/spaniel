a369c48d748290a4e9b3be8a24ab6877b454ec3c
Javier de la Hoz <javierdlahoz@gmail.com>
2021-01-18 05:59:41 +0100

    registering blocks properly

--- src/Registrars/BlockRegistrar.php
+++ src/Registrars/BlockRegistrar.php
@@ -7,8 +7,21 @@ use Jdlabs\Spaniel\Utils\Config;
 class BlockRegistrar implements RegistrarInterface
 {
 
+    private function registerBlocksTemplatesPath()
+    {
+        add_filter('block_lab_template_path', [$this, 'getBlockTemplatesPath']);
+    }
+
+    public function getBlockTemplatesPath(): string
+    {
+        $plugin_config = Config::get('plugin');
+        return ABSPATH . 'wp-content/plugins/' . $plugin_config['plugin_dir'] . '/partials';
+    }
+
     public function register(string $root_namespace = null)
     {
+        $this->registerBlocksTemplatesPath();
+
         $blocks = Config::get('blocks');
         foreach ($blocks as $block_key => $block) {
             block_lab_add_block($block_key, $block);
