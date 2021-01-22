6f6b36892d6dae3e094372dfe6e4b7988da08022
Javier de la Hoz <javierdlahoz@gmail.com>
2021-01-22 07:03:31 +0100

    allowing to use json and error methods on base controller

--- src/Controllers/BaseController.php
+++ src/Controllers/BaseController.php
@@ -12,4 +12,29 @@ use Jdlabs\Spaniel\Utils\Singleton;
 abstract class BaseController extends Singleton
 {
 
+    /**
+     * @param array|\WP_Error $response
+     * @param int $http_code
+     * @return \WP_REST_Response
+     */
+    public function json($response, int $http_code = 200): \WP_REST_Response
+    {
+        if (is_array($response)) {
+            return new \WP_REST_Response($response, $http_code);
+        } else if (get_class($response) === \WP_Error::class) {
+            return $this->error($response->get_error_message(), $response->get_error_code());
+        }
+
+        return $this->error('Response is not an array or WP_Error');
+    }
+
+    /**
+     * @param string $message
+     * @param int $http_code
+     * @return \WP_REST_Response
+     */
+    public function error(string $message, int $http_code = 500): \WP_REST_Response
+    {
+        return $this->json(['error' => $message], $http_code);
+    }
 }
